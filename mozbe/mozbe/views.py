7762bac7d015271c2232f3863beb3be8f8025886
vikram s <s.vikramprasanna@gmail.com>
2016-04-30 16:37:04 +0530

    fix url definition

--- mozbe/mozbe/views.py
+++ mozbe/mozbe/views.py
@@ -9,7 +9,7 @@ def api_root(request, format=None):
         ' Create Provider - ': reverse('provider-create', request=request, format=format),
         ' Areas List - ': reverse('area-list', request=request, format=format),
         ' Create Area - ': reverse('area-create', request=request, format=format),
-        ' AREA Batch operations: post/put/del - ': reverse('batch-operate-providers', request=request, format=format),
-        ' PROVIDER Batch operations: post/put/del - ': reverse('batch-operate-areas', request=request, format=format),
+        ' AREA Batch operations: post/put/del - ': reverse('batch-operate-areas', request=request, format=format),
+        ' PROVIDER Batch operations: post/put/del - ': reverse('batch-operate-providers', request=request, format=format),
     })
 
